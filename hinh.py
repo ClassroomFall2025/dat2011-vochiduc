3b7f655e399a7dde082cbf5d2619a713e38fdb83
vochiduc <voduc8806@gmail.com>
2025-10-15 18:52:04 +0700

    nộp lap 5

---
+++ hinh.py
@@ -0,0 +1,26 @@
+class hinh:
+    def __init__(self, dai, rong):
+        self.dai = dai
+        self.rong = rong
+    def tinh_chu_vi(self):
+        return (self.dai + self.rong) * 2
+    def tinh_dien_tich(self):
+        return self.dai * self.rong
+
+    def xuat(self):
+        print(f"chiều dài hình chữ nhật :{self.dai}")
+        print(f"chiều rộng hình chữ nhật :{self.rong}")
+        print(f"chu vi hình chữ nhật :{self.tinh_chu_vi()}")
+        print(f"diện tích hình chữ nhật :{self.tinh_dien_tich()}")
+        
+
+class hinhvuong (hinh):
+    def __init__(self, canh):
+        self.canh = canh
+        super().__init__(self.canh, self.canh)
+        
+    def xuat(self):
+        print(f"cạnh hình vuông :{self.canh}")
+        print(f"chu vi hình vuông :{self.tinh_chu_vi()}")
+        print(f"diện tích hình vuông :{self.tinh_dien_tich()}")
+    
